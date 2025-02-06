baec1ffb6a2bbb8703bbffb3dcbec6187c51ec28
ZhihuaSun <sunzhi@udel.edu>
2025-02-06 08:43:41 -0500

    new hw

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("There are 10 types of people in the world: those who understand binary and those who don't");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
